69c546218962f11c2231a4461542781466570b89
offrunts <kacper-jeznach2601@wp.pl>
2025-01-22 00:18:57 +0100

    hotfix - naprawiono zdjęcia i zwracanie null

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { JSDOM } = require('jsdom');  // Importujemy JSDOM
+const { JSDOM } = require('jsdom'); // Importujemy JSDOM
 const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
@@ -9,88 +9,129 @@ const PORT = 4000; // Port, na którym serwer będzie nasłuchiwał
 app.use(express.json());
 app.use(cors());
 
+async function autoScroll(page) {
+	await page.evaluate(async () => {
+		await new Promise((resolve) => {
+			let totalHeight = 0;
+			const distance = 200;
+			const timer = setInterval(() => {
+				const scrollHeight = document.body.scrollHeight;
+				window.scrollBy(0, distance);
+				totalHeight += distance;
+
+				if (totalHeight >= scrollHeight) {
+					clearInterval(timer);
+					resolve();
+				}
+			}, 100);
+		});
+	});
+}
+
 // Endpoint GET
 app.get('/', async (req, res) => {
-    let browser;
+	let browser;
 
-    try {
-        // Uruchomienie przeglądarki Puppeteer
-        browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+	try {
+		// Uruchomienie przeglądarki Puppeteer
+		browser = await puppeteer.launch({
+			args: ['--no-sandbox', '--disable-setuid-sandbox'],
+		});
 
-        // Otwarcie nowej strony
-        const page = await browser.newPage();
+		// Otwarcie nowej strony
+		const page = await browser.newPage();
 
-        // Pobranie parametru name z zapytania lub ustawienie domyślnej wartości
-        const searchQuery = req.query.name ? encodeURIComponent(req.query.name) : 'obuwie';
+		// Pobranie parametru name z zapytania lub ustawienie domyślnej wartości
+		const searchQuery = req.query.name ? encodeURIComponent(req.query.name) : 'obuwie';
 
-        // Zbieranie danych z butosklep.pl
-        await page.goto(`https://butosklep.pl/search.php?text=${searchQuery}`);
-        const butosklep = await page.evaluate(() => {
-            const rows = Array.from(document.querySelectorAll('.product'));
-            return rows.map((row) => ({
-                name: row.querySelector('img')?.alt || null,
-                img: row.querySelector('img')?.src || null,
-                price: row.querySelector('.price')?.textContent?.trim() || null,
-                href: row.querySelector('a.product__icon')?.href || null,
-            }));
-        });
+		// Zbieranie danych z butosklep.pl
+		await page.goto(`https://butosklep.pl/search.php?text=${searchQuery}`);
 
-        // Zbieranie danych z Moliera
-        await page.goto(`https://www.moliera2.com/product/index?q=${searchQuery}`);            
-        
-        const moliera = await page.evaluate(() => {
-            const rows = Array.from(document.querySelectorAll('.item'));
-            return rows.map((row) => ({
-                name: row.querySelector('img')?.alt || null,
-                img: row.querySelector('img')?.src || null,
-                price: row.querySelector('.sell-price')?.textContent?.trim() || null,
-                href: row.querySelector('a.product-list-item-a')?.href || null,
-            }));
-        });
+		await page.setViewport({
+			width: 1920,
+			height: 1080,
+		});
 
-        // Zbieranie danych z ccc.eu
-        await page.goto(`https://ccc.eu/pl/search?query[menu_item]=sr_&query[querystring]=${searchQuery}`);
-        await page.waitForSelector('.snrs-product');
-        
-        const ccc = await page.evaluate(() => {
-            const rows = Array.from(document.querySelectorAll('.snrs-product'));
-            return rows.map((row) => ({
-                name: row.querySelector('img')?.alt || null,
-                img: row.querySelector('img')?.src || null,
-                price: row.querySelector('.a-price')?.textContent?.trim() || null,
-                href: row.querySelector('a.a-typo')?.href || null,
-            }));
-        });
+		await autoScroll(page);
+		const butosklep = await page.evaluate(() => {
+			const rows = Array.from(document.querySelectorAll('.product'));
+			return rows.map((row) => ({
+				name: row.querySelector('img')?.alt || null,
+				img: row.querySelector('img')?.src || null,
+				price: row.querySelector('.price')?.textContent?.trim().split('zł')[0] + 'zł' || null,
+				href: row.querySelector('a.product__icon')?.href || null,
+			}));
+		});
 
-        // Filtrowanie wyników, aby usunąć puste nazwy w każdej grupie danych
-        const filteredData = {
-            butosklep: butosklep.filter(item => item.name),
-            moliera: moliera.filter(item => item.name),
-            ccc: ccc.filter(item => item.name),
-        };
+		// Zbieranie danych z Moliera
+		await page.goto(`https://www.moliera2.com/product/index?q=${searchQuery}`);
 
-        // Zamknięcie przeglądarki
-        await browser.close();
+		await page.setViewport({
+			width: 1920,
+			height: 1080,
+		});
 
-        // Zwrócenie wyników jako JSON
-        res.json(filteredData);
+		await autoScroll(page);
 
-    } catch (error) {
-        console.error('Błąd servera:', error);
+		const moliera = await page.evaluate(() => {
+			const rows = Array.from(document.querySelectorAll('.item'));
+			return rows.map((row) => ({
+				name: row.querySelector('img')?.alt || null,
+				img: row.querySelector('img')?.src || null,
+				price: row.querySelector('.price-widget > div')?.textContent?.trim() || null,
+				href: row.querySelector('a.product-list-item-a')?.href || null,
+			}));
+		});
 
-        // Wysyłamy odpowiedź o błędzie
-        res.status(500).send('Wystąpił błąd podczas scrapowania strony.');
-    } finally {
-        // Zawsze zamykamy przeglądarkę
-        if (browser) {
-            await browser.close();
-        }
-    }
+		// Zbieranie danych z ccc.eu
+		await page.goto(
+			`https://ccc.eu/pl/search?query[menu_item]=sr_&query[querystring]=${searchQuery}`
+		);
+		await page.waitForSelector('.snrs-product');
+
+		await page.setViewport({
+			width: 1920,
+			height: 1080,
+		});
+
+		await autoScroll(page);
+
+		const ccc = await page.evaluate(() => {
+			const rows = Array.from(document.querySelectorAll('.snrs-product'));
+			return rows.map((row) => ({
+				name: row.querySelector('img')?.alt || null,
+				img: row.querySelector('img')?.src || null,
+				price: row.querySelector('.a-price > div > span')?.textContent?.trim() || null,
+				href: row.querySelector('a.a-typo')?.href || null,
+			}));
+		});
+
+		// Filtrowanie wyników, aby usunąć puste nazwy w każdej grupie danych
+		const filteredData = {
+			butosklep: butosklep.filter((item) => item.name),
+			moliera: moliera.filter((item) => item.name),
+			ccc: ccc.filter((item) => item.name),
+		};
+
+		// Zamknięcie przeglądarki
+		await browser.close();
+
+		// Zwrócenie wyników jako JSON
+		res.json(filteredData);
+	} catch (error) {
+		console.error('Błąd servera:', error);
+
+		// Wysyłamy odpowiedź o błędzie
+		res.status(500).send('Wystąpił błąd podczas scrapowania strony.');
+	} finally {
+		// Zawsze zamykamy przeglądarkę
+		if (browser) {
+			await browser.close();
+		}
+	}
 });
 
 // Uruchamiamy serwer
 app.listen(PORT, () => {
-    console.log(`Serwer działa na porcie http://localhost:${PORT}`);
+	console.log(`Serwer działa na porcie http://localhost:${PORT}`);
 });
